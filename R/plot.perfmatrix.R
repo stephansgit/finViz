5deee44e80248fa4846a0d7897717f4f8cbfe83c
Stephan Sprenger <stephan.sprenger@gmx.de>
2014-08-26 20:33:42 +0200

    added roxygen2-documentation

--- R/plot.perfmatrix.R
+++ R/plot.perfmatrix.R
@@ -1,3 +1,12 @@
-plot.perfmatrix <- function() {
+#' Plotting a performance matrix
+#'
+#' This function plots the perfromances of finacial instruments in the form of a four-field-matrix
+#' @param data A zoo-object with returns
+#' @keywords financial performance
+#' @export
+#' @examples
+#' plot.perfmatrix()
+
+plot.perfmatrix <- function(data) {
 
 }
