4ab205118942c07d4fd78beb7215c1b364adb3bf
Stephan Sprenger <stephan.sprenger@gmx.de>
2014-08-26 20:59:40 +0200

    updated documentation

--- R/plot.perfmatrix.R
+++ R/plot.perfmatrix.R
@@ -1,6 +1,6 @@
 #' Plotting a performance matrix
 #'
-#' This function plots the perfromances of finacial instruments in the form of a four-field-matrix
+#' This function plots the performances of financial instruments in the form of a four-field-matrix
 #' @param data A zoo-object with returns
 #' @keywords financial performance
 #' @export
